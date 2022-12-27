1ef006660b597d88f2cd81ccef800b375368368f
haile-12-21-23 <hailemariamaddisu21@gmail.com>
2022-12-27 18:50:23 +0300

    logo component done

---
+++ src/components/AnimatedLetters/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+
+const AnimatedLetter = ({ letterClass, strArray, index }) => {
+    return (
+        <span>
+            {strArray.map((char, i) => (
+                <span key={char + i} className={`${letterClass} _${i + index}`}>
+                    {char}
+                </span>
+            ))}
+        </span>
+    )
+};
+export default AnimatedLetter
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,44 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png'
+import AnimatedLetter from '../AnimatedLetters';
 import './index.scss'
+import Logo from './Logo';
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['H', 'a', 'i', 'l', 'e', 'm', 'a', 'r', 'i', 'a', 'm'];
+    const jobArray = ['W', 'e', 'b', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.'];
+    // useEffect(() => {
+    //     return setTimeout(() => {
+    //         setLetterClass('text-animate-hover')
+    //     }, 400)
+    // }, [])
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>Hi, <br /> I am
-                    <img src={LogoTitle} alt="Developer" />
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i</span> <br />
+                        <span className={`${letterClass} _12`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                        <img
+                            src={LogoTitle}
+                            alt="JavaScript Developer Name, Web Developer Name"
+                        />
 
-                    Hailemariam
-                    <br />
-                    Web Developer </h1>
-                <h2 >Frontend Developer /Flutter</h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                        < AnimatedLetter letterClass={letterClass}
+                            strArray={nameArray} index={15} />
+                        <br />
+
+                        < AnimatedLetter letterClass={letterClass}
+                            strArray={jobArray} index={22} /> </h1>
+                    <h2 >Frontend Developer /Flutter</h2>
+                    <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                </div>
+                <Logo />
             </div>
-        </div>
+        </>
     )
 };
 
