b61fb45030d78cd58d58b4cca7e0d24eb304929a
haile-12-21-23 <hailemariamaddisu21@gmail.com>
2022-12-28 11:05:41 +0300

    start about

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,14 +3,14 @@ import SideBar from '../sidebar';
 import './index.scss';
 const Layout = () => {
     return (
-        <div className='App'> <SideBar />
-            <div className='page'>
-                <span className='tags top-tags'> &lt;body&gt;</span>
-                <Outlet />
+        <div className="App"> <SideBar />
+            <div className="page">
+                <span className="tags top-tags"> &lt;body&gt;</span>
 
-                <span className='tags bottom-tags'>&lt;body&gt;
+                <Outlet />
+                <span className="tags bottom-tags">&lt;/body&gt;
                     <br />
-                    <span className='bottom-tag-html'>&lt;/html&lt;</span>
+                    <span className="bottom-tag-html">&lt;/html&lt;</span>
                 </span>
 
             </div>
